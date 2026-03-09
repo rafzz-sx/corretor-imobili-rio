c2242e1aa25f0a50c58f315b9517023b8d23e99c
rafzz-sx <pereiraobrabo@gmail.com>
2026-03-09 16:27:03 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,31 +1,20 @@
-// ========== CONFIGURAÇÃO DO FIREBASE ==========
-// 
-// INSTRUÇÕES:
-// 1. Acesse https://console.firebase.google.com/
-// 2. Crie um novo projeto
-// 3. Clique no ícone de engrenagem ⚙️ > "Configurações do projeto"
-// 4. Em "Seus aplicativos", clique no ícone </> (Web)
-// 5. Copie o objeto firebaseConfig e cole abaixo
-// 6. Substitua TODO o conteúdo deste arquivo
-//
-// EXEMPLO DE CONFIGURAÇÃO:
-// const firebaseConfig = {
-//     apiKey: "AIzaSyABC123...",
-//     authDomain: "meu-projeto-12345.firebaseapp.com",
-//     projectId: "meu-projeto-12345",
-//     storageBucket: "meu-projeto-12345.appspot.com",
-//     messagingSenderId: "123456789012",
-//     appId: "1:123456789012:web:abcdef1234567890"
-// };
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "SUBSTITUA_AQUI_SUA_API_KEY",
-    authDomain: "SUBSTITUA_AQUI_SEU_PROJECT_ID.firebaseapp.com",
-    projectId: "SUBSTITUA_AQUI_SEU_PROJECT_ID",
-    storageBucket: "SUBSTITUA_AQUI_SEU_PROJECT_ID.appspot.com",
-    messagingSenderId: "SUBSTITUA_AQUI",
-    appId: "SUBSTITUA_AQUI"
+  apiKey: "AIzaSyCO9MlZmvUU-sDHokVO9fHkNStanrgWp4g",
+  authDomain: "corretor-imoveis-3dab8.firebaseapp.com",
+  projectId: "corretor-imoveis-3dab8",
+  storageBucket: "corretor-imoveis-3dab8.firebasestorage.app",
+  messagingSenderId: "967985829934",
+  appId: "1:967985829934:web:7565b4602dbe3ea5a267bf"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 // Exportar para uso nos outros arquivos
 if (typeof module !== 'undefined' && module.exports) {
